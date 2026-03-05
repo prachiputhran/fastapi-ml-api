87178d2eb686d34bb6dcdfa415273f158cd9c903
Prachi Puthran <prachiputhran12@gmail.com>
2026-03-05 12:14:41 +0530

    Fixed Dockerfile for Render dynamic port

--- app.py
+++ app.py
@@ -22,3 +22,7 @@ def predict(data: InputData):
     arr = np.array(data.features).reshape(1, -1)
     prediction = model.predict(arr)
     return {"prediction": int(prediction[0])}
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI ML API is running"}
