8ca5a5fb2c19db6d9bbc9c3b154a22cadea34617
Prachi Puthran <prachiputhran12@gmail.com>
2026-03-05 11:46:26 +0530

    FastAPI Docker ready

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import pickle
+import numpy as np
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Load model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+# Define input format (JSON)
+class InputData(BaseModel):
+    features: list
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI ML API is running"}
+
+@app.post("/predict")
+def predict(data: InputData):
+    arr = np.array(data.features).reshape(1, -1)
+    prediction = model.predict(arr)
+    return {"prediction": int(prediction[0])}
